925e1dd0feabf325cd03d3bf3a669b63e7b39053
robert <robert.udalov@gmail.com>
2018-03-26 00:42:05 +0200

    add target action

---
+++ Assets/RUTools/Systems/Action/TargetAction/Editor/TargetActionEditor.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using RUT.Editor;
+
+namespace RUT.Systems.Action
+{
+    [CustomEditor(typeof(TargetAction), true), CanEditMultipleObjects]
+    public class TargetActionEditor : RUToolsEditor<TargetAction>
+    {
+        private static readonly String _idSettingsExpanded = "SettingsExpanded";
+
+        private SerializedProperty _settingsProperty;
+        private SerializedProperty _collectorProperty;
+
+        private SerializedProperty _delayProperty;
+        private SerializedProperty _lifetimeProperty;
+        private SerializedProperty _repeatIntervalProperty;
+        private SerializedProperty _repeatLimitProperty;
+        private SerializedProperty _affectedLimitProperty;
+        private SerializedProperty _minTargetsForRepeatProperty;
+        private SerializedProperty _endOnNoRepeatLeftProperty;
+        private SerializedProperty _waitIfNotEnoughTargetsProperty;
+        private SerializedProperty _autoDisableProperty;
+        private SerializedProperty _repeatTypeProperty;
+
+        private bool _settingsExpanded = false;
+
+        private bool _lifeTimeActive = false;
+        private bool _affectedLimitActive = false;
+        private bool _repeatLimitActive = false;
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            _settingsProperty = serializedObject.FindProperty("settings");
+            _collectorProperty = serializedObject.FindProperty("collector");
+
+            _delayProperty = _settingsProperty.FindPropertyRelative("delay");
+            _lifetimeProperty = _settingsProperty.FindPropertyRelative("lifetime");
+            _repeatIntervalProperty = _settingsProperty.FindPropertyRelative("repeatInterval");
+            _repeatLimitProperty = _settingsProperty.FindPropertyRelative("repeatLimit");
+            _affectedLimitProperty = _settingsProperty.FindPropertyRelative("affectedLimit");
+            _minTargetsForRepeatProperty = _settingsProperty.FindPropertyRelative("minTargetsForRepeat");
+            _endOnNoRepeatLeftProperty = _settingsProperty.FindPropertyRelative("endOnNoRepeatLeft");
+            _waitIfNotEnoughTargetsProperty = _settingsProperty.FindPropertyRelative("waitIfNotEnoughTargets");
+            _autoDisableProperty = _settingsProperty.FindPropertyRelative("autoDisable");
+            _repeatTypeProperty = _settingsProperty.FindPropertyRelative("repeatType");
+
+            RUToolsPreferences preferences = RUToolsPreferences.GetPreferences();
+            _settingsExpanded = Convert.ToBoolean(preferences.GetEditorState(target.GetType(), _idSettingsExpanded));
+
+            _lifeTimeActive = _lifetimeProperty.floatValue >= 0 ? true : false;
+            _affectedLimitActive = _affectedLimitProperty.intValue >= 0 ? true : false;
+            _repeatLimitActive = _repeatLimitProperty.intValue >= 0 ? true : false;
+        }
+
+        public override void OnDisable()
+        {
+            base.OnDisable();
+
+            RUToolsPreferences preferences = RUToolsPreferences.GetPreferences();
+            preferences.SetEditorState(target.GetType(), _idSettingsExpanded, Convert.ToInt16(_settingsExpanded));
+        }
+
+        public override void DrawMainSettings()
+        {
+            _settingsExpanded = EditorGUILayout.Foldout(_settingsExpanded, ObjectNames.NicifyVariableName(_settingsProperty.name), true);
+
+            EditorGUI.indentLevel += 1;
+
+            if (_settingsExpanded)
+            {
+                //Delay
+                EditorGUILayout.PropertyField(_delayProperty);
+                if (_delayProperty.floatValue < 0)
+                    _delayProperty.floatValue = 0;
+
+                //LifeTime
+                _lifeTimeActive = DrawToggledField(_lifeTimeActive, () =>
+                {
+                    EditorGUILayout.LabelField(ObjectNames.NicifyVariableName(_lifetimeProperty.name), "Unlimited");
+                    _lifetimeProperty.floatValue = -1;
+                }, () =>
+                {
+                    EditorGUILayout.PropertyField(_lifetimeProperty);
+                    if (_lifetimeProperty.floatValue < 0)
+                        _lifetimeProperty.floatValue = 0;
+                });
+
+                //Repeat
+                EditorGUILayout.LabelField("Repeat");
+                EditorGUI.indentLevel += 1;
+
+                //AffectedLimit
+                _affectedLimitActive = DrawToggledField(_affectedLimitActive, () =>
+                {
+                    EditorGUILayout.LabelField(ObjectNames.NicifyVariableName(_affectedLimitProperty.name), "Unlimited");
+                    _affectedLimitProperty.intValue = -1;
+                }, () =>
+                {
+                    EditorGUILayout.PropertyField(_affectedLimitProperty);
+                    if (_affectedLimitProperty.intValue < 0)
+                        _affectedLimitProperty.intValue = 0;
+                });
+
+                //RepeatTime
+                DrawButtonedField(() =>
+                {
+                    GenericMenu menu = new GenericMenu();
+
+                    TargetAction.UpdateType currentType = ((TargetAction.UpdateType)_repeatTypeProperty.enumValueIndex);
+                    Array enumList = Enum.GetValues(typeof(TargetAction.UpdateType));
+
+                    foreach(TargetAction.UpdateType t in enumList)
+                    {
+                        TargetAction.UpdateType type = t;
+                        bool active = currentType == type;
+
+                        menu.AddItem(new GUIContent(t.ToString()), active, () =>
+                        {
+                            _repeatTypeProperty.enumValueIndex = (int)type;
+                            serializedObject.ApplyModifiedProperties();
+                        });
+                    }
+
+                    menu.ShowAsContext();
+                }, () =>
+                {
+                    switch((TargetAction.UpdateType)_repeatTypeProperty.enumValueIndex)
+                    {
+                        case TargetAction.UpdateType.Update:
+                            EditorGUILayout.LabelField(ObjectNames.NicifyVariableName(_repeatIntervalProperty.name), "Update");
+                            break;
+                        case TargetAction.UpdateType.FixedUpdate:
+                            EditorGUILayout.LabelField(ObjectNames.NicifyVariableName(_repeatIntervalProperty.name), "FixedUpdate");
+                            break;
+                        default:
+                            EditorGUILayout.PropertyField(_repeatIntervalProperty);
+                            if (_repeatIntervalProperty.floatValue < 0)
+                                _repeatIntervalProperty.floatValue = 0;
+                            break;
+                    }
+                });
+
+                //RepeatLimit
+                _repeatLimitActive = DrawToggledField(_repeatLimitActive, () =>
+                {
+                    EditorGUILayout.LabelField(ObjectNames.NicifyVariableName(_repeatLimitProperty.name), "Unlimited");
+                    _repeatLimitProperty.intValue = -1;
+                }, () =>
+                {
+                    EditorGUILayout.PropertyField(_repeatLimitProperty);
+                    if (_repeatLimitProperty.intValue < 0)
+                        _repeatLimitProperty.intValue = 0;
+
+                });
+
+                if (_repeatLimitActive)
+                {
+                    //EndOnNoRepeatLeft
+                    EditorGUILayout.PropertyField(_endOnNoRepeatLeftProperty);
+                }
+
+                //MinTargetsForRepeat
+                EditorGUILayout.PropertyField(_minTargetsForRepeatProperty);
+                if (_minTargetsForRepeatProperty.intValue < 0)
+                    _minTargetsForRepeatProperty.intValue = 0;
+                //WaitIfNotEnoughTargets
+                EditorGUILayout.PropertyField(_waitIfNotEnoughTargetsProperty);
+
+                EditorGUI.indentLevel -= 1;
+
+                //AutoDisable
+                EditorGUILayout.PropertyField(_autoDisableProperty);
+            }
+
+            EditorGUI.indentLevel -= 1;
+
+            //EditorGUILayout.PropertyField(_settingsProperty, true);
+            EditorGUILayout.PropertyField(_collectorProperty, true);
+        }
+
+        public override void DrawAdditionalSettings()
+        {
+            base.DrawAdditionalSettings();
+        }
+    }
+}
---
+++ Assets/RUTools/Systems/Action/TargetAction/TargetAction.cs
@@ -0,0 +1,304 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using RUT.Tools.Collision;
+using RUT.Tools.Collision.Collector;
+
+namespace RUT.Systems.Action
+{
+    /// <summary>
+    /// Targeted action base class. Used to call actions on collected targets.
+    /// </summary>
+    public class TargetAction : MonoBehaviour
+    {
+        #region Public properties
+        public Settings settings = new Settings(0,1,-1,-1,-1);
+        public TargetCollector collector;
+
+        public delegate void CallbackAction (List<ITargetable> targets);
+        public event CallbackAction OnStart;
+        public event CallbackAction OnLoop;
+        public event CallbackAction OnEnd;
+        #endregion
+
+        #region Private properties
+        protected MonoBehaviour _currentUser = null;
+        private Coroutine _actionCR = null;
+
+        private YieldInstruction _yieldFrame = new WaitForEndOfFrame();
+        private YieldInstruction _yieldFixed = new WaitForFixedUpdate();
+
+        private List<ITargetable> _currentTargets = new List<ITargetable>();
+        private Dictionary<ITargetable, int> _affectedTargets = new Dictionary<ITargetable, int>();
+        #endregion
+
+        #region API
+        /// <summary>
+        /// Starts action.
+        /// </summary>
+        public void Use(MonoBehaviour user)
+        {
+            if(!gameObject.activeSelf)
+                gameObject.SetActive(true);
+
+            _currentUser = user;
+            StartAction();
+        }
+
+        /// <summary>
+        /// Stops action.
+        /// </summary>
+        public void Stop()
+        {
+            StopAction();
+            StopAllCoroutines();
+
+            if (settings.autoDisable)
+                gameObject.SetActive(false);
+        }
+        #endregion
+
+        #region Unity
+        protected virtual void Start()
+        {
+            if (settings.autoDisable)
+                gameObject.SetActive(false);
+        }
+        #endregion
+
+        #region Private methods
+        protected virtual void OnStartCallback(List<ITargetable> targets) { }
+        protected virtual void OnLoopCallback(List<ITargetable> targets) { }
+        protected virtual void OnEndCallback(List<ITargetable> targets) { }
+
+        /// <summary>
+        /// Handle the data reset and on end events.
+        /// </summary>
+        protected void StopAction(bool raiseEvent = true)
+        {
+            if (_actionCR != null)
+            {
+                this.StopCoroutineSafe(ref _actionCR);
+
+                if (raiseEvent)
+                {
+                    UpdateCurrentTargets(false);
+
+                    if(OnEnd != null)
+                        OnEnd(_currentTargets);
+
+                    OnEndCallback(_currentTargets);
+                }
+            }
+
+            _affectedTargets.Clear();
+            _currentTargets.Clear();
+        }
+
+        /// <summary>
+        /// Starts the action's process.
+        /// </summary>
+        protected void StartAction()
+        {
+            StopAction(false);
+            _actionCR = StartCoroutine(ActionProcess());
+        }
+
+        private IEnumerator ActionProcess()
+        {
+            if (settings.delay > 0)
+                yield return new WaitForSeconds(settings.delay);
+
+            UpdateCurrentTargets(false);
+
+            if(OnStart != null)
+                OnStart(_currentTargets);
+
+            OnStartCallback(_currentTargets);
+
+            float lastUpdateTime = Time.time;
+            float lifeTime = settings.lifetime;
+            int repeatCount = settings.repeatLimit;
+
+            //Callback is called only if repeat limit is > 0 or negative,
+            bool callbackActive = repeatCount != 0;
+
+            YieldInstruction yieldOnUpdate;
+            switch (settings.repeatType)
+            {
+                case UpdateType.Update:
+                    yieldOnUpdate = _yieldFrame;
+                    break;
+                case UpdateType.FixedUpdate:
+                    yieldOnUpdate = _yieldFixed;
+                    break;
+
+                default:
+                    yieldOnUpdate = new WaitForSeconds(settings.repeatInterval);
+                    break;
+            }
+
+            while (true)
+            {
+                yield return yieldOnUpdate;
+
+                bool endRequest = false;
+                bool enoughTargets = false;
+
+                do
+                {
+                    //If lifetime is not infinite, check for ending.
+                    if (lifeTime >= 0)
+                    {
+                        lifeTime -= Time.time - lastUpdateTime;
+                        lastUpdateTime = Time.time;
+
+                        if (lifeTime < 0)
+                        {
+                            endRequest = true;
+                            break;
+                        }
+                    }
+
+                    UpdateCurrentTargets(true);
+
+                    enoughTargets = _currentTargets.Count >= settings.minTargetsForRepeat;
+
+                    //if action doesn't need to wait or there is enough targets, break.
+                    if (!settings.waitIfNotEnoughTargets || enoughTargets)
+                        break;
+
+                    yield return _yieldFixed;
+                } while (!enoughTargets);
+
+                if (enoughTargets)
+                {
+                    if (callbackActive)
+                    {
+                        if (OnLoop != null)
+                            OnLoop(_currentTargets);
+
+                        OnLoopCallback(_currentTargets);
+                    }
+
+                    //If repeat limit is not infinite check count.
+                    if (repeatCount >= 0)
+                    {
+                        repeatCount--;
+
+                        //If repeat limit has been reached, disable callback.
+                        if (repeatCount <= 0)
+                        {
+                            repeatCount = 0;
+                            callbackActive = false;
+                            if (settings.endOnNoRepeatLeft)
+                                endRequest = true;
+                        }
+                    }
+                }
+
+                if (endRequest)
+                    break;
+            }
+
+            StopAction();
+        }
+
+        private void UpdateCurrentTargets(bool notifyAffection)
+        {
+            _currentTargets.Clear();
+
+            if (collector != null)
+            {
+                ITargetable[] potentialTargets = collector.Collection;
+
+                if (settings.affectedLimit > 0)
+                {
+                    for (int i = 0; i < potentialTargets.Length; ++i)
+                    {
+                        int affectedCount;
+
+                        if (_affectedTargets.TryGetValue(potentialTargets[i], out affectedCount))
+                        {
+                            if (affectedCount < settings.affectedLimit)
+                            {
+                                if(notifyAffection)
+                                    _affectedTargets[potentialTargets[i]]++;
+
+                                _currentTargets.Add(potentialTargets[i]);
+                            }
+                        }
+                        else
+                        {
+                            if(notifyAffection)
+                            {
+                                affectedCount = 1;
+                                _affectedTargets.Add(potentialTargets[i], affectedCount);
+                            }
+
+                            _currentTargets.Add(potentialTargets[i]);
+                        }
+                    }
+                }
+                //If no affected limit, use on everyone.
+                else if (settings.affectedLimit < 0)
+                {
+                    _currentTargets.AddRange(potentialTargets);
+                }
+            }
+        }
+        #endregion
+
+        #region SubType
+        [System.Serializable]
+        public struct Settings
+        {
+            //Delay before starting action.
+            public float delay;
+            //Time during which the action will be active. Negative value = unlimited.
+            public float lifetime;
+            //Action's repeat interval time.
+            public float repeatInterval;
+
+            //Type of repeat synchronisation.
+            public UpdateType repeatType;
+            //Number of times that action can be used before ending. Negative value = unlimited.
+            public int repeatLimit;
+            //Number of times that a target can be affected by the action. Negative value = unlimited.
+            public int affectedLimit;
+
+            //Min targets count for calling repeat callback.
+            public int minTargetsForRepeat;
+
+            //If action should stop when repeat limit has been reached.
+            public bool endOnNoRepeatLeft;
+            //If action should wait until it can call the repeat callback before looping.
+            public bool waitIfNotEnoughTargets;
+            //If object should be automatically disabled at the end.
+            public bool autoDisable;
+
+            public Settings(float delay, float lifetime, float repeatInterval,
+                int repeatLimit, int affectedLimit)
+            {
+                this.delay = delay;
+                this.lifetime = lifetime;
+                this.repeatInterval = repeatInterval;
+                this.repeatLimit = repeatLimit;
+                this.affectedLimit = affectedLimit;
+                this.minTargetsForRepeat = 0;
+                this.endOnNoRepeatLeft = false;
+                this.waitIfNotEnoughTargets = false;
+                this.autoDisable = false;
+                this.repeatType = 0;
+            }
+        }
+
+        public enum UpdateType : int
+        {
+            Time = 0,
+            Update,
+            FixedUpdate,
+        }
+        #endregion
+    }
+}
