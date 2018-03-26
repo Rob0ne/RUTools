5d64145cc51f7baa4e0d5709d9a84d2bc6d8b471
robert <robert.udalov@gmail.com>
2018-03-27 00:55:04 +0200

    add target part editor

---
+++ Assets/RUTools/Tools/Collision/Editor/TargetPartEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using RUT.Editor;
+
+namespace RUT.Tools.Collision
+{
+    [CustomEditor(typeof(TargetPart), true), CanEditMultipleObjects]
+    public class TargetPartEditor : RUToolsEditor<TargetPart>
+    {
+        public override void DrawMainSettings()
+        {
+            DrawPropertiesExcluding(serializedObject, _scriptFieldName);
+        }
+    }
+}
